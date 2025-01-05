1c8c8f1dcd7cedc74b7c2919aa47f06c8e21e8ce
Mailen Zino <mailenzino15@gmail.com>
2025-01-05 11:33:49 -0300

    Paper ent y ePublicado

--- BLL/PAPER.cs
+++ BLL/PAPER.cs
@@ -4,22 +4,16 @@ using System.Text;
 
 namespace BLL
 {
-    public class PAPER {
-        private uint id;
-        private string Titulo;
-        private string Link;
-
-        #region GETTERS & SETTERS
-        public uint GetId() { return id; }
-        public void SetId(uint _id) { id = _id; return; }
-        public string GetTitulo() { return Titulo; }
-        public void SetTitulo(string titulo) { Link = titulo; return; }
-        public string GetLink() { return Link; }
-        public void SetLink(string link) { Link = link; return; }
-        #endregion
-
-        public PAPER() { 
-
+    namespace BLL_Entidades
+    {
+        public class PAPER_Ent
+        {
+            private uint id {get; set};
+            private string Titulo  {get; set};
+            private string Link  {get; set};
+            private ePUBLICADO Publicado  {get; set};
         }
+
     }
+   
 }
---
+++ BLL/ePUBLICADO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL
+{
+	namespace BLL_Entidades
+    {
+		public enum ePUBLICADO : int
+		{
+			Privado,
+			Publicado
+		}
+	}
+	
+}
+
